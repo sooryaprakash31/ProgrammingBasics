74721294dc0975d35f870b4f4ba286cb56b22583
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-04 13:35:34 +0530

    Searching - binary.py

---
+++ Searching/Binary/binary.py
@@ -0,0 +1,47 @@
+'''
+Binary Search:
+
+Compares the key with the middle element and
+i) if the key is greater than middle element,
+    compares key with the middle element of the right block
+ii) if the key is lesser than middle element,
+    compares key with the middle element of the left block
+Repeats this process recursively
+
+- Works only with sorted array inputs
+'''
+
+def binary_search(arr,l,h,key):
+    
+    #Always l must be less than h, 
+    #otherwise leads to out of bounds error
+    if l<h:
+        
+        #calculating middle element of the array
+        mid=(l+h)//2
+
+        #if key is equal to mid, return mid
+        if key==arr[mid]:
+            return mid
+
+        #calls binary_search for left block, if key is lesser
+        elif key<arr[mid]:
+            return binary_search(arr,l,mid,key)
+        
+        #calls binary_search for right block, if key is greater
+        else:
+            return binary_search(arr,mid+1,h,key)
+
+    #if l is greater than h, it means all elements have been compared
+    else:
+        return 0
+
+arr=list(map(int,input("Enter array ").split()))
+key=int(input("Enter key "))
+
+#calling binary_search function
+result=binary_search(arr,0,len(arr)-1,key)
+if result==0:
+    print("Not found")
+else:
+    print("Key found at position ",result+1)
