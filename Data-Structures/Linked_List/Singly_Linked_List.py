2d71835b833571c3932cd37d72ebc40b86f544ab
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-11 11:25:41 +0530

    Singly Linked List

--- Data-Structures/Linked_List/Singly_Linked_List.py
+++ Data-Structures/Linked_List/Singly_Linked_List.py
@@ -1,5 +1,3 @@
-
-
 class Node:
     def __init__(self,data=None):
         self.data=data
@@ -7,39 +5,106 @@ class Node:
     
 class SinglyLinkedList:
     def __init__(self):
-        self.head=Node()
-
+        self.head=None
+    
+    #appends the node at the end of the list
     def append(self, node):
-        temp = self.head
-        if temp.data==None:
+        if self.head is None:
             self.head=node
+            print("Appended!")
         else:
+            temp=self.head
             while temp.next!=None:
                 temp=temp.next
             temp.next=node
+            print("Appended!")
 
-    def printAll(self):
+    #prepends the node at the beginning of the list
+    def prepend(self, node):
+        node.next = self.head
+        self.head=node
+        print("Prepended!")
+
+    #inserts node at a position
+    def insertAt(self, node, pos):
         temp = self.head
-        if temp.data==None:
+        if self.head is None:
+            print("Position does not exist")
+            return
+        else:
+            count=1
+            while count<pos:
+                temp = temp.next
+                if not temp:
+                    print("Position does not exist")
+                    return
+                count+=1
+            node.next = temp.next
+            temp.next = node
+            print("Inserted!")
+
+    #deletes node at a position
+    def deleteAt(self,pos):
+        temp=self.head
+        if self.head is None:
+            print("Position does not exist")
+            return
+        else:
+            count=1
+            while count<pos:
+                temp = temp.next
+                if temp is None:
+                    print("Position does not exist")
+                    return
+                count+=1
+            nextNode = temp.next
+            temp.next = nextNode.next
+            print("Deleted!")
+
+    #returns the length of the list
+    def getLength(self):
+        temp = self.head
+        total=0
+        while temp:
+            total+=1
+            temp = temp.next
+        print("Length ",total)
+
+    #prints all the nodes in the list
+    def printAll(self):
+        if self.head is None:
             print("Empty List")
         else:
-            while temp!=None:
-                print("print", temp.data)
+            temp = self.head
+            while temp:
+                print(temp.data, sep=' ')
                 temp=temp.next
 
-s=SinglyLinkedList()
 
+s=SinglyLinkedList()
 choice = 0
-while choice!=6:
-    print("1.Append\t2.Prepend\t3.InsertAt\t4.DeleteAt\t5.PrintAll\t6.Stop")
+while True:
+    print("1.Append\t2.Prepend\t3.InsertAt\t4.DeleteAt\t5.PrintAll\t6.Length\t7.Stop")
     choice = int(input("Enter choice: "))
     node = Node()
     if choice==1:
         node.data = int(input("Enter data "))
         s.append(node)
-        break
+    elif choice==2:
+        node.data = int(input("Enter data "))
+        s.prepend(node)
+    elif choice==3:
+        node.data = int(input("Enter data "))
+        pos = int(input("Enter position "))
+        s.insertAt(node,pos)
+    elif choice==4:
+        pos = int(input("Enter position "))
+        s.deleteAt(pos)
     elif choice==5:
         s.printAll()
-        pass
     elif choice==6:
+        s.getLength()
+    elif choice==7:
         break
+    else:
+        print("Invalid choice")
