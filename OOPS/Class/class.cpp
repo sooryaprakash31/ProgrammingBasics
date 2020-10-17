6e2d76d725c175c22d3e71edadd09ffed3a1f254
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-17 20:24:31 +0530

    OOPS - Class

---
+++ OOPS/Class/class.cpp
@@ -0,0 +1,58 @@
+/*
+Class:
+
+    - Class is a user defined datatype which holds its own 
+    members - variables and functions. 
+    - The members of a class can be accessed using objects.
+
+Object:
+    - Object is an entity with some characteristics and behaviour.
+    - Object is an instance of a class and used to access the members of a class.
+    - Memory for a class is allocated only when the class is instantiated 
+      i.e object for the class is created
+
+*/
+
+#include<iostream>
+using namespace std;
+
+// 'class' keyword is used while creating the class
+class Person{
+    
+    //private members can be accessed only within the class
+    private:
+        string name;
+        int age;
+
+    //public members can be accessed from anywhere
+    public:
+        
+        //constructor gets called when memory is allocated for the class
+        Person(){
+            cout<<"Constructor\n";
+        }
+
+        void getDetails(){
+            cout<<"Enter name and age ";
+            cin>>name>>age;
+        }
+
+        void printDetails(){
+            cout<<"\nName: "<<name<<" Age: "<<age;
+        }
+        //destructor gets called when memory is destroyed for the class
+        ~Person(){
+            cout<<"\nDestructor";
+        }
+};
+
+int main(){
+
+    //person is the object for the class Person
+    Person person;
+    
+    person.getDetails();
+    person.printDetails();
+
+    return 0;
+}
