01789ba8e4eadd0bc7cbbde97f37493a8067db6f
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-20 22:25:12 +0530

    DS - Linked List DLL Python

--- Data-Structures/Linked_List/Doubly/doubly.py
+++ Data-Structures/Linked_List/Doubly/doubly.py
@@ -12,7 +12,7 @@ class DoublyLinkedList:
         self.head = None
     
     #appends the node at the end of the list
-    def append(self, node):
+    def appendNode(self, node):
         if self.head is None:
             self.head = node
         else:
@@ -22,6 +22,13 @@ class DoublyLinkedList:
             temp.next = node
             node.previous = temp
         print("Appended!")
+    
+    #prepends the node at the beginning of the list
+    def prependNode(self,node):
+        node.next = self.head
+        node.previous = None
+        self.head = node
+        print("Prepended!")
 
     #prints all the nodes in the list
     def printAll(self):
@@ -40,14 +47,17 @@ d = DoublyLinkedList()
 choice = 0
 
 while True:
-    print("\n1.Append\t4.PrintAll\t5.Stop")
+    print("\n1.Append\t2.Prepend\t4.PrintAll\t5.Stop")
     choice = int(input("Enter choice: "))
     node = Node()
 
-    if choice ==1:
+    if choice == 1:
         node.data = int(input("Enter data: "))
-        d.append(node)
-    elif choice ==4:
+        d.appendNode(node)
+    elif choice == 2:
+        node.data = int(input("Enter data: "))
+        d.prependNode(node)
+    elif choice == 4:
         d.printAll()
     else:
         break
