e8bba67f74feabe904a9b963427f5facde64a2af
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-23 09:17:06 +0530

    OOPS - Polymorphism - Runtime

--- OOPS/Polymorphism/C++/Runtime/function_overriding.cpp
+++ OOPS/Polymorphism/C++/Runtime/function_overriding.cpp
@@ -1,8 +1,29 @@
 /*
+Polymorphism:
 
+    - Polymorphism - Many forms
+    - It allows an entity such as a variable, a function, or an object to have more than one form.
+
+Runtime Polymorphism:
+
+    - Runtime polymorphism is also known as dynamic polymorphism or late binding. 
+    - In runtime polymorphism, the function call is resolved at run time.
+    - It is achieved by function overriding
+
+Function Overrriding:
+    
+    - It enables you to provide specific implementation of the function which is already provided by its base class.
+    - It is achieved by using virtual functions
+
+Virtual Functions;
+
+    - The idea is that virtual functions are called according to the type of the object instance pointed to or referenced, 
+      not according to the type of the pointer or reference. 
+    - uses 'virtual' keyword
 */
 
-
+//The following example explains Function overriding using virtual functions
+ 
 #include<iostream>
 using namespace std;
 
@@ -10,14 +31,17 @@ class Base{
 
     public:
 
+        //non-virtual function
         void fun1(){
             cout<<"\nBase function 1";
         }
         
+        //virtual function
         virtual void fun2(){
             cout<<"\nBase function 2";
         }
 
+        //virtual function
         virtual void fun3(){
             cout<<"\nBase function 3";
         }
@@ -26,6 +50,9 @@ class Base{
 class Derived:public Base{
 
     public:
+
+        //derived class has definitions for already defined functions in base class
+        //here fun2() and fun3() are overridden
 
         void fun1(){
             cout<<"\nDerived function 1";
@@ -42,17 +69,37 @@ class Derived:public Base{
 
 int main(){
     
+    //creating a pointer of type Base
     Base * base_ptr;
+
+    //creating an object for Derived
     Derived derived_ptr;
 
+    //now base_ptr, pointer of type Base holds the address of object of derived
     base_ptr = &derived_ptr;
 
+    //virtual functions work in a way that the compiler calls the functions
+    //based on the type of object referenced (type of derived_ptr is Derived) and 
+    //not based on the type of actual pointer (i.e type of base_ptr is Base)
+
+    //fun1 is non-virtual. Hence, Base class function is called
     base_ptr->fun1();
 
+    //fun2 is virtual. Derived class function is called
     base_ptr->fun2();
 
+    //Eventhough fun3 is virtual, there is no exact prototype for fun3 in derived class
+    //Hence, Base class function is called
     base_ptr->fun3();
-
 
     return 0;
 }
+
+/*
+Output:
+
+Base function 1
+Derived function 2
+Base function 3
+
+*/
