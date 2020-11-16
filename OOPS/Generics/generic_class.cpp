272c615342412bf88148c98c7762e7d07b6b551d
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-11-16 22:58:03 +0530

    OOPS - Generics

--- OOPS/Generics/generic_class.cpp
+++ OOPS/Generics/generic_class.cpp
@@ -1,17 +1,39 @@
+/*
+Generics:
+
+    - Generic programming allows to perform the same function/algorithm for
+      all data types
+    - Eliminates the need to create different functions for different data types
+      to execute the same algorithm
+    - Achieved using templates
+        i) Function templates
+        ii) Class templates
+
+    Advantages:
+        - Code Reusability
+        - Avoid function overloading
+
+*/
+
+
 #include<iostream>
 using namespace std;
 
+//creates a template class Arithmetic
 template <typename T> class Arithmetic{
 
     private:
         T x,y;
     
     public:
-        Arithmetic(T num1, T num2){
+        //receives two numbers num1 and num2
+        Arithmetic(T num1, T num2){ 
+
             x=num1;
             y=num2;
         }
 
+        //adds x and y
         void add(){
             cout<<"The addition of "<<x<<" and "<<y<<" is "<<x+y<<endl;          
         }
@@ -23,10 +45,11 @@ int main(){
     Arithmetic<int> intAdd(2,3);
     Arithmetic<float> floatAdd(2.5,3.5);
 
+    //adds the integer numbers 2 and 3
     intAdd.add();
 
+    //adds the float numbers 2.5 and 3.5
     floatAdd.add();
-
 
     return 0;
 }
--- OOPS/Generics/generic_function.cpp
+++ OOPS/Generics/generic_function.cpp
@@ -1,8 +1,28 @@
+/*
+Generics:
+
+    - Generic programming allows to perform the same function/algorithm for
+      all data types
+    - Eliminates the need to create different functions for different data types
+      to execute the same algorithm
+    - Achieved using templates
+        i) Function templates
+        ii) Class templates
+
+    Advantages:
+        - Code Reusability
+        - Avoid function overloading
+
+*/
+
 #include <iostream>
 using namespace std;
 
-template <typename S> S Max(S x, S y)
+//creates template functions Max
+template <typename S>
+S Max(S x, S y)
 {
+    //returns the maximum number
     return x > y ? x : y;
 }
 
@@ -10,10 +30,15 @@ int main()
 {
 
     cout << "The biggest number between\n";
-    cout << "i) 5 and 6 is " << Max<int>(5, 6)<<endl;
 
-    cout << "ii) 5.7 and 4.7 is " << Max<float>(5.7, 4.7)<<endl;
-    cout << "iii) a and b is "<<Max<char>('a', 'b')<<endl;
+    //performs Max for integers 5 and 6
+    cout << "i) 5 and 6 is " << Max<int>(5, 6) << endl;
+
+    //performs Max for float 5.7 and 4.7
+    cout << "ii) 5.7 and 4.7 is " << Max<float>(5.7, 4.7) << endl;
+
+    //performs Max for characters a and b
+    cout << "iii) a and b is " << Max<char>('a', 'b') << endl;
 
     return 0;
 }
