52899de9ca650dcc21435cf1dc4843d54afe1f0a
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-12-21 21:34:20 +0530

    Algorithms - Graph - BFS & DFS

--- Algorithms/Graph/BFS/bfs.cpp
+++ Algorithms/Graph/BFS/bfs.cpp
@@ -1,42 +1,82 @@
+/*
+Breadth First Search (BFS):
+    - Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures. 
+    It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a 'search key'), 
+    and explores all of the neighbour nodes at the present depth prior to moving on to the nodes at the next depth level.
+
+Steps:
+    i) Visit a node V.
+    ii) Find the adjacent nodes(W) of the node (V) and Visit all of them.
+    iii) Visit a node (V) from the adjacent nodes (W).
+    iv) Repeat ii and iii until there is no adjacent node(W) for a node (V). 
+
+Algorithm:
+    i) Enqueue the start node to the queue
+    ii) Dequeue the queue and mark it as visited
+    iii) Find the adjacent nodes for the marked node, enqueue them to the queue 
+         and mark them as visited
+    iv) Repeat ii and iii until the queue is empty
+
+*/
+
+
 #include<iostream>
 #include<list>
+#include<queue>
 using namespace std;
 
 class Graph{
 
+    //number of vertices/nodes
     int v;
     list<int> *adj;
     
     public:
         Graph(int v){
             this->v = v;
+            
+            //list of v lists for creating the adjaceny list
             adj = new list<int>[v];
         }
 
         void addEdge(int v, int w){
             
+            //adjacent node W is added for node V
             adj[v].push_back(w);
         }
 
-        void BFS(int s){
+        void BFS(int start){
+
+            //visited list to store visited status
             bool *visited = new bool[v];
-            list<int> queue;
+
+            //initializing false for all nodes
             for(int i =0; i<v;i++)
                 visited[i] = false;
+            
+            //queue structure to store the traversal
+            queue<int> q;
+            q.push(start);
+            cout<<start<<"\t";
+            
 
-            queue.push_back(s);
-            cout<<s<<"\t";
-            visited[s]=true;
+            //iterator to traverse through a single node in the adjaceny list
             list<int>::iterator i;
 
-            while(!queue.empty()){
-            
-                int temp = queue.front();
-                queue.pop_front();
+            while(!q.empty()){
+                
+                //step ii from algorithm
+                int temp = q.front();
+                q.pop();
+                visited[temp]=true;
+
+                //step iii from algorithm
                 for(i = adj[temp].begin(); i!= adj[temp].end();i++){
                     if(!visited[*i]){
                         visited[*i] = true;
-                        queue.push_back(*i);
+                        q.push(*i);
+
+                        //visiting a vertex/node
                         cout<<*i<<"\t";
                     }
                 }
@@ -48,14 +88,19 @@ class Graph{
 
 int main(){
 
-
+    //instantiating Graph with number of vertices
     Graph g(4);
+
+    //Adding edges to the graph
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 2);
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
+
+    //calling BFS method with start vertex/node
     g.BFS(2);
+
     return 0;
 }
--- Algorithms/Graph/DFS/dfs.cpp
+++ Algorithms/Graph/DFS/dfs.cpp
@@ -1,39 +1,79 @@
+/*
+Depth First Search (DFS):
+    - Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures. 
+      The algorithm starts at the root node (selecting some arbitrary node as the root node in the case of a graph) 
+      and explores as far as possible along each branch before backtracking.
+
+Steps:
+    i) Visit the vertex V.
+    ii) Finds its adjacent nodes (W) and visit any of these nodes (V).
+    iii) Repeat i and ii until there is no adjacent node(W) for a node(V). 
+
+Algorithm:
+    i) Push the starting node to the stack
+    ii) Pop the top from the stack and mark it as visited
+    iii) Find all the adjacent nodes from the marked node and push them to the stack
+    iv) Repeat ii and iii until the stack is empty
+
+*/
+
+
 #include<iostream>
 #include<stack>
 #include<list>
 
 using namespace std;
 
-
 class Graph{
+
+    //number of vertices/nodes
     int v;
     list<int> *adj;
+
     public:
 
         Graph(int v){
             this->v = v;
+
+            //list of v lists for creating the adjaceny list
             adj = new list<int>[v];  
         }
 
         void addEdge(int v, int w){
+
+            //adjacent node W is added for node V
             adj[v].push_back(w);
         }
 
         void DFS(int start){
 
-            stack<int> s;
-            bool *visited = new bool[v];
 
+            //visited list to store visited status
+            bool *visited = new bool[v];
+            
+            //initializing false for all nodes
             for(int i=0;i<v;i++)
                 visited[i]=false;
+            
+            //stack structure to store the traversal
+            stack<int> s;
             s.push(start);
 
+            //iterator to traverse through a single node in the adjaceny list
             list<int>::iterator i;
+            
+            
             while(!s.empty()){
+                
+                //step ii from algorithm
                 int temp = s.top(); 
                 s.pop();
                 visited[temp]=true;
+
+                //visiting a vertex/node
                 cout<<temp<<"\t";
+                
+                //step iii from algorithm
                 for(i = adj[temp].begin();i!=adj[temp].end();i++ ){
                     if(!visited[*i]){
                         s.push(*i);
@@ -46,13 +86,19 @@ class Graph{
 
 int main(){
 
+    //instantiating Graph with number of vertices
     Graph g(4);
+
+    //Adding edges to the graph
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 2);
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
+
+    //calling DFS method with start node/vertex
     g.DFS(0);
+
     return 0;
 }
