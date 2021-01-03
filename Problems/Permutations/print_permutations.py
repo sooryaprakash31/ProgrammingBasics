9f6563800933ef3e2efe3bc6df9275f641cce0bd
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2021-01-03 12:26:23 +0530

    Problems - Permutations

--- Problems/Permutations/print_permutations.py
+++ Problems/Permutations/print_permutations.py
@@ -1,14 +1,23 @@
 def permute(a,l,r,result):
+
+    # print a if l is equal to r
     if l==r:
         print(a)
+
     else:
+
+        #loop from l to r
         for i in range(l,r):
-            a[l],a[i] = a[i],a[l]
-            permute(a,l+1,r,result)
+
+            #swap first index with element at i
             a[l],a[i] = a[i],a[l]
 
+            #run permute for remining values keeping element at l as fixed
+            permute(a,l+1,r,result)
+
+            #backtracking
+            a[l],a[i] = a[i],a[l]
+
+
 l=list(map(int,input().split()))
-result=[]
-permute(l,0,len(l),result)
-for i in result:
-    print(i)
+permute(l,0,len(l))
