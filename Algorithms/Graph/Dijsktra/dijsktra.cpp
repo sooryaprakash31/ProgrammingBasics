08f26b9f73dce13cd47c8af5fb3fd3df52a7e816
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2021-01-03 11:53:47 +0530

    Algorithms - Graph - Dijsktra

--- Algorithms/Graph/Dijsktra/dijsktra.cpp
+++ Algorithms/Graph/Dijsktra/dijsktra.cpp
@@ -12,6 +12,7 @@ using namespace std;
 #define V 9
 #define max 999
 
+//returns the minimum unprocessed index in the dist array
 int minDistance(int dist[], bool pSet[]){
 
     int min = max, min_index;
@@ -28,27 +29,44 @@ int minDistance(int dist[], bool pSet[]){
 
 void dijsktra(int graph[V][V], int start){
 
+    //creates an integer array of size V to store the distances from the source 
+    //and bool array of size V to store the processed status 
     int dist[V];
     bool pSet[V];
+
+    //initialize dist array with max value and pSet array with false
     for(int i=0;i<V;i++){
         dist[i] = max;
         pSet[i] = false;
     }
 
+    //always distance of source/start is zero
     dist[start] = 0;
 
+    
     for(int i=0; i<V-1; i++){
-
+        //assign the minimum distance from the dist array to u
         int u = minDistance(dist, pSet);
+        cout<<u;
+        //mark u as processed
         pSet[u] = true;
 
+        //loop from 0 to V to update the dist values of the vertices adjacent to u
         for(int v=0; v<V; v++){
+            
+            //The following conditions to be satisfied
+            //1. There must be an edge between u and v
+            //2. The sum of distance from source to v must be less than the distance in v
+            //3. v should not be processed already
+            //4. Distance of u must not be equal to max
+
             if(
                 graph[u][v] && 
                 dist[u] + graph[u][v] < dist[v] &&
                 !pSet[v] &&
                 dist[u] != max 
             ){
+                //assign the new minimum distance to dist[v]
                 dist[v] = dist[u] + graph[u][v];
             }
         }
