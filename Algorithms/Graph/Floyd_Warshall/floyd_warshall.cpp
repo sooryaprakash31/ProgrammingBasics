73fb684e686d5fd02db9b18340234d370b42f881
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2021-01-05 15:04:06 +0530

    Algorithms - Graph - Floyd Warshall

--- Algorithms/Graph/Floyd_Warshall/floyd_warshall.cpp
+++ Algorithms/Graph/Floyd_Warshall/floyd_warshall.cpp
@@ -1,28 +1,51 @@
+/*
+Floyd Warshall:
+
+    - Finds the shortest distances between every pair of vertices in 
+      a given edge weighted directed graph
+    
+Algorithm:
+
+    i) create dist array and initialize with graph array values
+    ii) loop k to V (All vertices as intermediate one by one)
+            loop i to V (All vertices as source one by one)
+                loop j to V (All vertices as destination one by one)
+                    update the distance if the distance is greated than the 
+                    sum of distances of i to k and k to j
+                    
+
+*/
+
+
 #include<iostream>
 using namespace std;
 #define INF 9999
 
 void floydWarshall(int graph[4][4]){
-
+    //create dist array to store the shortest distances
     int dist[4][4], i,j,k;
 
+    //initialize the dist array with the values from graph array
     for(i=0;i<4;i++){
         for(j=0;j<4;j++){
             dist[i][j] = graph[i][j];
         }
     }
-
+    //find if there is any intermediate vertex (k) between i and j
+    //and compare the distance with the sum of i to k and k to j
+    //Update the distance if the distance through intermediate vertex is lesser
     for ( k = 0; k < 4; k++)
     {
         for(i=0;i<4;i++){
             for(j=0;j<4;j++){
                 
+                //compares the sum of i to k and k to j with the old distance
                 if(dist[i][k]+dist[k][j] <dist[i][j])
                     dist[i][j] = dist[i][k]+dist[k][j];
             }
         }
     }
-    
+    //print the dist array which contains shortest distances 
     for(i=0;i<4;i++){
         for(j=0;j<4;j++){
             if(dist[i][j]==INF){
